cfe4675fef3ec7f11fded12dc8cd85e635c7cad8
Gabriela Rivero <regaby@gmail.com>
2020-09-08 00:48:46 -0300

    [ADD] module l10n_moldeo_default

---
+++ l10n_moldeo_default/__manifest__.py
@@ -0,0 +1,43 @@
+{
+    'name': 'l10n_moldeo',
+    'version': '13.0.0.0',
+    'category': 'Tools',
+    'summary': "Proyecto Localización Argentina de Moldeo",
+    'author': 'Ing. Gabriela Rivero',
+    'depends': [
+        'base',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+
+    'limit_request': '8196',
+    'limit_memory_soft': '640000000',
+    'limit_memory_hard': '760000000',
+    'limit_time_cpu': '60',
+    'limit_time_real': '120',
+
+    # manifest version, if omitted it is backward compatible
+    'env-ver': '2',
+
+    # if Enterprise it installs in a different directory than community
+    'odoo-license': 'CE',
+
+    # port where odoo starts serving pages
+    'port': '8069',
+
+    # list of url repos to install in the form 'repo-url directory'
+    'git-repos': [
+        'https://github.com/regaby/cl-l10n_moldeo.git',
+        'https://github.com/regaby/odoo-custom.git',
+        ## localización
+        'https://github.com/ctmil/odoo-argentina.git',
+    ],
+
+    'docker-images': [
+        'odoo regaby/odoo-ce:13.0',
+        'postgres postgres:10.1-alpine',
+        'nginx nginx'
+    ]
+}
